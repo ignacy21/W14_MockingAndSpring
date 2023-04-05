bc342b2814f2bae5d2036a73899d45d2664fd119
ignacyszpilowski <ignacyszpilowski@gmail.com>
2023-04-05 15:23:45 +0200

    Mockito BDD fixed

--- src/test/java/pl/example/UserRegistryServiceTest.java
+++ src/test/java/pl/example/UserRegistryServiceTest.java
@@ -3,11 +3,14 @@ package pl.example;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 public class UserRegistryServiceTest {
 
     @InjectMocks
