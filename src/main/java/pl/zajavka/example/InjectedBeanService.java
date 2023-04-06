f4deccc455fccac94826c42043d7505c8cb4ea94
ignacyszpilowski <ignacyszpilowski@gmail.com>
2023-04-06 15:48:14 +0200

    Spring Application Testing - integrated tests

---
+++ src/main/java/pl/zajavka/example/InjectedBeanService.java
@@ -0,0 +1,6 @@
+package pl.zajavka.example;
+
+
+public interface InjectedBeanService {
+    boolean anotherSampleMethod();
+}
---
+++ src/main/java/pl/zajavka/example/InjectedBeanServiceImpl.java
@@ -0,0 +1,14 @@
+package pl.zajavka.example;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class InjectedBeanServiceImpl implements InjectedBeanService {
+
+
+    @Override
+    public boolean anotherSampleMethod() {
+        System.out.println("InjectedBeanServiceImpl # anotherSampleMethod()");
+        return false;
+    }
+}
